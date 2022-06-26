a38b32e873722e39305e13280f0c0714ce3bc65e
minocys <ryan@crypto.com>
2022-06-26 15:03:10 +0200

    add socials

--- components/index.tsx
+++ components/index.tsx
@@ -1,2 +1,3 @@
 export { default as Container } from "./Container";
 export * from "./Container";
+export * from "./Socials";
--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -21,3 +21,39 @@ test("it should display my current position", async ({ page }) => {
 
   await expect(subheading).toHaveText("Technical Lead at Crypto.com");
 });
+
+test("it should have a link to linkedin", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  const link = page.locator("a.linked-in");
+
+  await expect(link).toBeVisible();
+
+  const href = await link.getAttribute("href");
+
+  await expect(href).toMatch("https://linkedin.com/in/ryleung");
+});
+
+test("it should have a link to github", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  const link = page.locator("a.github");
+
+  await expect(link).toBeVisible();
+
+  const href = await link.getAttribute("href");
+
+  await expect(href).toMatch("https://github.com/minocys");
+});
+
+test("it should have a mailto link", async ({ page }) => {
+  await page.goto("http://localhost:3000");
+
+  const link = page.locator("a.mail-to");
+
+  await expect(link).toBeVisible();
+
+  const href = await link.getAttribute("href");
+
+  await expect(href).toMatch("mailto: contact@ryan-leung.com");
+});
