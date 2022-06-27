8539ac25548631bf1e46eb448d777fd05ae1b4b7
Ryan Leung <minocys@users.noreply.github.com>
2022-06-27 18:10:57 +0200

    Styles (#4)
    * Update hero content
    
    * remove footer
    
    * style home page
    
    * remove container component
    
    * crossOrigin typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Container, LinkedIn, Mail, GitHub } from "../components";
+import { LinkedIn, Mail, GitHub } from "../components";
 
 const Home: NextPage = () => {
   return (
@@ -10,25 +10,36 @@ const Home: NextPage = () => {
         <title>Ryan Leung</title>
         <meta name="description" content="ryan-leung.com" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&family=Overpass:wght@700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className="flex flex-column">
-        <Container>
-          <h1>Ryan Leung</h1>
-          <h2>Technical Lead at Crypto.com</h2>
-        </Container>
-        <Container>
-          <LinkedIn />
-          <GitHub />
-          <Mail />
-        </Container>
-      </main>
+      <main className="flex flex-col w-full h-screen">
+        <div className="flex flex-row justify-center items-center h-screen w-screen absolute">
+          <h1 className="flex flex-col">
+            <span className="font-bold font-sans text-4xl">Hello!</span>
+            <span className="font-serif text-3xl">
+              My name is <span className="font-bold">Ryan</span> :)
+            </span>
+          </h1>
+        </div>
 
-      <footer>
-        <a href="/" target="_blank" rel="noopener noreferrer">
-          Home
-        </a>
-      </footer>
+        <footer className="flex flex-row justify-center fixed inset-x-0 bottom-0 mb-10">
+          <div className="mx-2">
+            <LinkedIn />
+          </div>
+          <div className="mx-2">
+            <GitHub />
+          </div>
+          <div className="mx-2">
+            <Mail />
+          </div>
+        </footer>
+      </main>
     </div>
   );
 };
